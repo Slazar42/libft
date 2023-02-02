b05c3f5a382620fb6e797d029836674b831359de
Slazar42 <104556311+Slazar42@users.noreply.github.com>
2023-02-02 23:38:08 +0100

    fix usage of the del attribute

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -16,14 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
 	t_list	*temp;
+	void	*contentcpy;
 
 	head = NULL;
 	while (lst)
 	{
-		temp = ft_lstnew((*f)(lst->content));
+		contentcpy = f(lst->content);
+		temp = ft_lstnew(contentcpy);
+		if (!temp)
+			del(contentcpy);
 		ft_lstadd_back(&head, temp);
 		lst = lst->next;
 	}
 	return (head);
-	del(0);
 }
+
+// newlist = ft_lstmap(mylist, ft_strdup, free);
+// dna Polymerase
